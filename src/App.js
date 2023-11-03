8ae753c8e6508746fcf3f154cff436cbe7de8283
Samuel Andersson <58047562+samswede@users.noreply.github.com>
2023-11-03 22:15:25 +0100

    add app

---
+++ src/App.js
@@ -0,0 +1,50 @@
+
+import './App.css';
+import NavBar from './components/pages/home/Nav/NavBar';
+import ActionArea from './components/pages/home/ActionArea/ActionArea.js';
+
+function App() {
+  return (
+    <>
+      <header>
+        <NavBar websiteTitle={'The Network State'}/>
+      </header>
+      
+      <main>
+        <section id="action-section">
+          <img id="main-concept-image">
+          </img>
+          <ActionArea />
+        </section>
+        <section id="description">
+          <h2>Description</h2>
+          <p>Technology has enabled us to start new companies, 
+            new communities, and new currencies. 
+            But can we use it to start new cities, or even new countries? 
+            This book explains how to build the successor to the nation state, 
+            a concept we call the network state.</p>
+        </section >
+          <ActionArea />
+          <ActionArea />
+          <ActionArea />
+          <ActionArea />
+        <section id="content-section">
+
+        </section>
+      </main>
+
+
+      <footer>
+      </footer>
+    </>
+  );
+}
+
+export default App;
+
+
+
+
+function yourClickHandler() {
+  console.log('Hello!');
+}
